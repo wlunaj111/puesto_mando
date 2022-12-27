c6665198940cce6e7534b60f645f2cc69666e034
wluna <wlunaj111@gmail.com>
2022-12-27 16:47:54 -0500

    agregando STATIC_ROOT

--- puesto_mando/settings.py
+++ puesto_mando/settings.py
@@ -127,6 +127,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
